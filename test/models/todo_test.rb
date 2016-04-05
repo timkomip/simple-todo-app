d1203420be5b503bf33b74620cfcf25ef435d414
Michael Timko <timkomip@gmail.com>
2016-04-04 20:37:36 -0400

    add failing test

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'a empty todo should be invalid' do 
+
+    refute Todo.new.valid?
+  end
 end
